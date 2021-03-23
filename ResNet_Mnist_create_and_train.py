e864bb23bdb9fd1dcd053eea174ac14d74eabf6b
MarcelloCeresini <mceresini.97@gmail.com>
2021-03-23 17:29:01 +0100

    commented

--- ResNet_Mnist_create_and_train.py
+++ ResNet_Mnist_create_and_train.py
@@ -1,5 +1,5 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import tensorflow_datasets as tfds
 from tensorflow.keras import layers
