b384b4d370a42dcc21ecc411ee734d8e05a46691
volpepe <volpepe.prog@gmail.com>
2021-03-27 12:58:37 +0100

    Updated .gitignore Added some comments on the first example

--- Asperti Notebooks/DL1_FirstNN.py
+++ Asperti Notebooks/DL1_FirstNN.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
-
-
+ 
+# Hidden function that the network has to learn and approximate progressively
 def myhiddenfunction(x):
     return (np.sin(x)**2 + np.cos(x)/3 + 1)/2
 
-
+# Dataset generator
 def generator(batchsize):
     while True:
         inputs = np.random.uniform(low=-np.pi, high=np.pi, size=batchsize)
@@ -16,19 +16,27 @@ def generator(batchsize):
             outputs[i] = myhiddenfunction(inputs[i])
         yield inputs, outputs
 
-
+# Definition of the network: 
+# It's a simple network with three hidden dense layers
+# and a single output neuron.
 input_layer = layers.Input(shape=(1,))
 x = layers.Dense(20, activation="relu")(input_layer)
 x = layers.Dense(30, activation="relu")(x)
 x = layers.Dense(20, activation="relu")(x)
 output_layer = layers.Dense(1, activation="relu")(x)
-
 mymodel = tf.keras.models.Model(input_layer, output_layer)
+
+# Print a summary of the model
 mymodel.summary()
+
+# Compile the model
 mymodel.compile(optimizer="adam", loss="mse")
+
+# Fit the model using the generator as dataset
 batchsize = 64
 mymodel.fit_generator(generator(batchsize), steps_per_epoch=1000, epochs=10)
 
+# Plot the learnt function over the real one
 x = np.arange(-np.pi, np.pi, 0.05)
 y = [myhiddenfunction(a) for a in x]
 z = [z[0] for z in mymodel.predict(np.array(x))]
