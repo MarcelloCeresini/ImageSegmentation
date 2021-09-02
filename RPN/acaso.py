074bf6fc0b00226c0fea1a5840e286f977366ed1
MarcelloCeresini <mceresini.97@gmail.com>
2021-09-02 10:42:40 +0200

    deleted "acaso.py"

--- RPN/acaso.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import utils_functions as utils
-
-ACCEPTED_CLASSES_IDS = [[1, 2, 3], [4, 7], [9]]
-old_new_ids = utils.group_classes(ACCEPTED_CLASSES_IDS)
-
-for a in annotations:
-    mask = tf.where(old_new_ids[:,0] == a)
-    a = int(tf.gather(old_new_ids[:,1], mask))
